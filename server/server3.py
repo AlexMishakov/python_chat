a83382a5cb9d8e4515c1bb0d0bbbc4e8ce2646ff
Alex <34554369+AlexMishakov@users.noreply.github.com>
2022-04-14 11:34:11 +0300

    duplicate server2 to server3

---
+++ server/server3.py
@@ -0,0 +1,19 @@
+import socket
+import threading
+
+def listen_user(conn):
+    data = conn.recv(1024)
+    print(str(data))
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('', 55000))
+sock.listen(10)
+print('Server is running, please, press ctrl+c to stop')
+
+while True:
+    conn, addr = sock.accept()
+
+    print('connected:', addr)
+
+    conn_thread = threading.Thread(target=listen_user, args=(conn,))
+    conn_thread.start()
